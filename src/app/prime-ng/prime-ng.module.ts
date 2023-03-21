5c55864b2c82613720517f127de89f45699a58ac
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-21 12:28:41 -0600

    creación de Módulo personalizado para componentes PrimeNG

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//PrimeNG
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports:[
+    ButtonModule,
+    CardModule
+  ]
+})
+export class PrimeNgModule { }
