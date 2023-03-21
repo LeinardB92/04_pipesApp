83023344d22f68e346d6b76778b032368680234c
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-21 17:18:51 -0600

    Cambiando las rutas des del Menubar y personalizar el mismo Menubar

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MenuItem} from 'primeng/api';
 
 @Component({
@@ -6,34 +6,39 @@ import {MenuItem} from 'primeng/api';
   templateUrl: './menu.component.html',
   styleUrls: []
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
   items: MenuItem[] = [];
 
-  ngOnInit() {
+  constructor(){};
+
+  ngOnInit(): void {
     this.items = [
-        {
-            label: 'File',
-            items: [{
-                    label: 'New', 
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {label: 'Project'},
-                        {label: 'Other'},
-                    ]
-                },
-                {label: 'Open'},
-                {label: 'Quit'}
-            ]
-        },
-        {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-            ]
-        }
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numeros'
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'no-comunes'
+          }
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog'
+      }
     ];
-}
+  }
 }
